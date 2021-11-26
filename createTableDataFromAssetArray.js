9695a547e7c5082de1c51d63564b85190c9ca681
jogeorgeou <85650601+jogeorgeou@users.noreply.github.com>
2021-11-26 18:20:45 -0500

    Inclusion of Swap Fees or APR
    Swap Fees or APR Cell - can be changed to either depending on interest
    - Note this value is only true based upon a 1 year realization of the APR. 
    - Worth discussing if we want to leave that disclaimer or use a means of adjusting for 1 day hold, 1 week, 1 month etc.
    
    Small changes:
    - Background of table changed to standard dark mode, 2 decimals on number values, attempted white table text using MuiTableCell override

---
+++ createTableDataFromAssetArray.js
@@ -0,0 +1,29 @@
+import { calculateILFromAssetArray } from "./calculateILFromAssetArray";
+
+//Helper function to create an asset value table from assetArray
+export function createTableDataFromAssetArray(assetArray, investment, SwapFee) {
+
+    //Mapper function to create rows
+    function createData(assetName, initialValue, valueIfHeld, valueWithIL, valueWithFees) {
+        return { assetName, initialValue, valueIfHeld, valueWithIL, valueWithFees};
+      }
+
+    //Local variables  
+    const copy = [...assetArray];
+    const impLoss = calculateILFromAssetArray(copy);
+    let rows = [];
+    let assetValueIfHeld = 0;
+    let ilOfAsset = 0;
+
+    //Data row creation
+    for (let i = 0; i < copy.length; i++) {
+        assetValueIfHeld = ( 1 + 1 * Number(copy[i].priceChange / 100)) * Number(copy[i].poolWeights) / 100 * investment;
+        ilOfAsset = Number(assetValueIfHeld - assetValueIfHeld * (impLoss / 100)).toFixed(2);
+        const initialValue = copy[i].poolWeights / 100 * investment
+        const valueWithFees = Number((assetValueIfHeld - assetValueIfHeld * (impLoss / 100)) * (1 + SwapFee/100));
+        const row = createData(copy[i].assetName, initialValue, assetValueIfHeld, ilOfAsset, valueWithFees);
+        rows.push(row);
+    }
+    console.log("rows", rows);
+    return rows;
+}
