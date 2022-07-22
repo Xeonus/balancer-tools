b061973c41a2ab3af7e3ed400251eb6fa4b15ec7
Xeonus <vanderbourg@gmail.com>
2022-07-22 17:00:10 +0200

    fixing corrupted folder structure
    bugfix to remove non capital folders before the newest feature merge.

---
+++ src/components/pages/TokenCalculator/TokenCalculator.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Header from "../../UI/Header";
+import Grid from '@mui/material/Grid';
+import { Box } from '@mui/material';
+import TokenCalculatorForm from "./TokenCalculatorForm";
+import VeBalLogo from './../../../resources/veBAL.png'
+import Alert from '@mui/material/Alert';
+
+export default function TokenCalculator(props) {
+    return (
+        <div key={props.networkId} >
+            <Grid item xs={12}>
+                <Header>veBAL Token Estimator</Header>
+                <Box >
+                    <img src={VeBalLogo} alt="veBAL Logo" width="120" />
+                </Box>
+            </Grid>
+            {props.networkId === 'ethereum' ?
+                <Grid item xs={12} justifyContent="center">
+                    <TokenCalculatorForm darkState = {props.darkState} networkId = {props.networkId}></TokenCalculatorForm>
+                </Grid>
+                :
+                <Box p={0.5} display="flex" flexDirection="row" justifyContent="center">
+                <Alert severity="info">
+                    Chain does not support veBAL!
+                </Alert>
+                </Box>
+                
+            }
+        </div>
+    );
+}
